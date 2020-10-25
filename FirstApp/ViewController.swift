b3786f70d35556148426d612c76548d8b32c90d4
amatsukixprog <amatsukixprog@gmail.com>
2020-10-26 02:26:58 +0900

    hello swiftアプリケーション

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // (1)ラベルに文字を挿入する
+        outputLabel.text = "Hello Swift!"
     }
 
 
+    @IBOutlet weak var outputLabel: UILabel!
 }
 
